944c253926d78af0bb331da5d08a5a061e6dc118
Marko Kovač <124689071+markokovac16@users.noreply.github.com>
2026-07-18 15:36:52 +0200

    footer + router setup

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
   <Header />
+  <main class="px-12 py-3">
+    <RouterView />
+  </main>
+  <Footer />
 </template>
 
 <style scoped></style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex w-full h-16 px-12 py-3 justify-between items-center fixed bottom-0 shadow-xl">
+    <div class="items-center w-1/3">
+      <p class="text-bold text-3xl text-sb-blue">StudyBuddy</p>
+    </div>
+
+    <div class="flex gap-6 items-center justify-center w-1/3">
+      <p>O nama</p>
+      <p>Kontakt</p>
+      <p>Privatnost</p>
+      <p>Uvjeti</p>
+    </div>
+
+    <p class="flex justify-end items-center w-1/3">© 2024 StudyBuddy Kognitivno utočište</p>
+  </div>
+</template>
+
+<script setup></script>
